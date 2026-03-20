accc41618b928a4cd794a37c69f62040499cc91d
Potmot <377029227@qq.com>
2026-03-20 18:39:33 +0800

    refactor(examples): 重构基础示例应用结构
    - 修改包名称从 base-example 为 @potmot/list-base-example
    - 移除默认的 README.md 文件内容
    - 删除 Vite 配置中的路径别名设置
    - 升级 Vite 依赖从版本 7.3.1 到 8.0.1

--- examples/base-example/src/App.vue
+++ examples/base-example/src/App.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-import ViewListExample from './components/ViewListExample.vue';
-import EditListExample from './components/EditListExample.vue';
+import ViewListBase from './components/ViewListBase.vue';
+import EditListBase from './components/EditListBase.vue';
+import {ref} from 'vue';
+
+const examples = {
+    ViewListBase: {
+        label: "ViewList Base",
+        component: ViewListBase,
+    },
+    EditListBase: {
+        label: "EditList Base",
+        component: EditListBase,
+    },
+} as const;
+
+type ExampleType = keyof typeof examples;
+
+const example = ref<ExampleType>('ViewListBase');
 </script>
 
 <template>
-    <ViewListExample/>
-    <EditListExample/>
+    <div>
+        <select v-model="example">
+            <option v-for="(example, key) in examples" :key="key" :value="key">
+                {{ example.label }}
+            </option>
+        </select>
+        <component :is="examples[example].component" />
+    </div>
 </template>
--- examples/base-example/src/main.ts
+++ examples/base-example/src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './style/base.css'
-import '@potmot/list/index.css'
+import '@potmot/list/dist/index.css'
 
 createApp(App).mount('#app')
